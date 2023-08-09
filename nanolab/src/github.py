869e06aa520ddaefe40960c9d11ffd768508e7f5
gr0vity-dev <iq.cc@pm.me>
2023-08-09 23:00:10 +0200

    Allow the usage of a branch reference in the gh-path

--- nanolab/src/github.py
+++ nanolab/src/github.py
@@ -1,5 +1,3 @@
-# nanolab/command/github.py
-
 import requests
 import base64
 
@@ -9,27 +7,38 @@ class GitHubAPI:
     def __init__(self, github_user, github_repo, testcase_path):
         self.github_user = github_user
         self.github_repo = github_repo
-        self.testcase_path = testcase_path
-        self.api_url = self.construct_github_api_url()
 
-    def construct_github_api_url(self):
-        if self.testcase_path == "default":
-            self.testcase_path = "default"
-        return f"https://api.github.com/repos/{self.github_user}/{self.github_repo}/contents/{self.testcase_path}"
+        # Extract ref from testcase_path and update it
+        self.testcase_path, self.gh_ref = self.extract_ref_from_path(
+            testcase_path)
+
+    def get_api_url(self, endpoint):
+        url = f"https://api.github.com/repos/{self.github_user}/{self.github_repo}/contents/{endpoint}{self.gh_ref}"
+        print(url)
+        return url
+
+    def extract_ref_from_path(self, testcase_path):
+        if "?ref=" in testcase_path:
+            parts = testcase_path.split("?ref=")
+
+            # Return testcase_path (without ref) and the ref value
+            return parts[0], "?ref=" + parts[1]
+        else:
+            # Return the original testcase_path and an empty string for gh_ref
+            return testcase_path, ""
 
     def get_files(self):
-        print(self.api_url)
-        response = requests.get(self.api_url)
+
+        response = requests.get(self.get_api_url(self.testcase_path))
         if response.status_code == 200:
             return response.json()
         else:
             print(
-                "Error retrieving the repository contents. Please check the provided URL."
-            )
+                "Error retrieving the repository contents. Please check the provided URL.")
             return None
 
     def get_file_content(self, file_name):
-        file_api_url = f"{self.api_url}/{file_name}"
+        file_api_url = self.get_api_url(f"{self.testcase_path}/{file_name}")
         response = requests.get(file_api_url)
 
         if response.status_code == 200:
@@ -37,6 +46,5 @@ class GitHubAPI:
             return base64.b64decode(file_content).decode("utf-8")
         else:
             print(
-                "Error retrieving the file. Please check the provided URL and file name."
-            )
+                "Error retrieving the file. Please check the provided URL and file name.")
             return None

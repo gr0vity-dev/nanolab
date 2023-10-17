913ad7ca3a6e44374c09ad916359ab08df6f7616
gr0vity-dev <iq.cc@pm.me>
2023-10-17 09:14:52 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="nanolab",
-      version="0.0.12",
+      version="0.0.14",
       author="gr0vity",
       description="testing tool using nanomock",
       long_description=long_description,
@@ -12,7 +12,8 @@ setup(name="nanolab",
       url="https://github.com/gr0vity-dev/nanolab",
       packages=find_packages(exclude=["unit_tests"]),
       include_package_data=True,
-      install_requires=["nanomock>=0.0.14", "sqlalchemy", "aiohttp", "nanolog_parser"],
+      install_requires=["nanomock>=0.0.14",
+                        "sqlalchemy", "aiohttp", "nanolog_parser"],
       entry_points={
           'console_scripts': [
               'nanolab=nanolab.main:main',

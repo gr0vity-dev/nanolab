d95fe849855f0d8d35e5f33b150dbb37cf2c9de3
gr0vity-dev <iq.cc@pm.me>
2023-10-17 08:22:05 +0200

    feat: allow undefined variable
    - variables in a snippet that are not defined are initialised to en empty string

--- nanolab/command/mixins/bash_command_mixin.py
+++ nanolab/command/mixins/bash_command_mixin.py
@@ -10,8 +10,8 @@ class BashCommandMixin(CommandMixinBase):
 
     def execute(self):
         variables = self._get_completed_variables()
-        command = self.command_instance.command_config['command'].format(
-            **variables)
+        command = self.safe_format(
+            self.command_instance.command_config['command'], **variables)
         process = subprocess.run(
             command, shell=True, text=True, capture_output=True)
 
@@ -23,3 +23,9 @@ class BashCommandMixin(CommandMixinBase):
             response = process.stdout.strip() or process.stderr.strip(
             ) or command
             print(response)
+
+    def safe_format(self, template, **kwargs):
+        class SafeDict(dict):
+            def __missing__(self, key):
+                return ''
+        return template.format_map(SafeDict(**kwargs))
